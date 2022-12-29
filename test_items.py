d727ae17da2c00f4fe07cd9aa5b6e1a1d92bd404
User11525884 <64400740+User11525884@users.noreply.github.com>
2022-12-29 17:38:59 +0300

    change name function

--- test_items.py
+++ test_items.py
@@ -6,7 +6,7 @@ import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_guest_should_see_login_link_pass(browser):
+def test_btn_name_correct_lenguage(browser):
     browser.get(link)
 
     time.sleep(30)
