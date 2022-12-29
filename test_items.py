b6a435819c27baa7d8cc3f537bd227171980f221
User11525884 <64400740+User11525884@users.noreply.github.com>
2022-12-29 17:31:00 +0300

    Add 2 files
    Add config file - conftest.py and main file - test_items.py

---
+++ test_items.py
@@ -0,0 +1,15 @@
+# [FAILED] pytest -v --language=es test_items.py
+# [PASSED] pytest -v --language=fr test_items.py 
+
+from selenium.webdriver.common.by import By
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_guest_should_see_login_link_pass(browser):
+    browser.get(link)
+
+    time.sleep(30)
+
+    button_value = browser.find_element(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket").text
+    assert button_value == "Ajouter au panier", f"Надпись на кнопке '{button_value}', а должна быть - 'Ajouter au panier',"
